69b4c31d4693635f642eb9d47d3e2893235b8363
Edwin Bartunek <edwin@devhart.io>
2019-10-01 13:15:36 +0900

    Organized component folder, added form-input, signin and users page

---
+++ src/pages/users/users.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component'
+import './users.styles.scss';
+
+const Users = () => (
+    <div className='sign-in-and-sign-up'>SIGN IN
+    <SignIn/>
+    </div>
+
+);
+
+export default Users;
