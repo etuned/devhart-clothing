ee5716572535c5f58af44254fa82182ce7a50c47
Edwin Bartunek <edwin@devhart.io>
2022-09-23 01:05:39 +0900

    mobile friendly card payment

--- src/components/payment-form/payment-form.styles.tsx
+++ src/components/payment-form/payment-form.styles.tsx
@@ -11,7 +11,13 @@ export const PaymentFormContainer = styled.div`
 
 export const FormContainer = styled.form`
   height: 100px;
-  min-width: 500px;
+    @media screen and (max-width: 800px) {
+    min-width: 500px;
+  }
+  @media screen and (min-width: 400px) {
+    min-width: 300px;
+  }
+
 `;
 
 export const PaymentButton = styled(Button)`
